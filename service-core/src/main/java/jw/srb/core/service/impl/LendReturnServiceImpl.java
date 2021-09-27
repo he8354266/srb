5da2d904588a70b7f562f9dc86b14d3c36fb796a
heyang <453159575@qq.com>
2021-09-27 14:10:46 +0800

    .

--- service-core/src/main/java/jw/srb/core/service/impl/LendReturnServiceImpl.java
+++ service-core/src/main/java/jw/srb/core/service/impl/LendReturnServiceImpl.java
@@ -9,19 +9,26 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jw.srb.common.exception.Assert;
 import jw.srb.common.result.ResponseEnum;
+import jw.srb.core.enums.LendStatusEnum;
+import jw.srb.core.enums.TransTypeEnum;
 import jw.srb.core.hfb.FormHelper;
 import jw.srb.core.hfb.HfbConst;
 import jw.srb.core.hfb.RequestHelper;
 import jw.srb.core.mapper.*;
+import jw.srb.core.pojo.bo.TransFlowBO;
 import jw.srb.core.pojo.entity.Lend;
+import jw.srb.core.pojo.entity.LendItem;
+import jw.srb.core.pojo.entity.LendItemReturn;
 import jw.srb.core.pojo.entity.LendReturn;
 import jw.srb.core.service.*;
+import jw.srb.core.util.LendNoUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -118,5 +125,75 @@ public class LendReturnServiceImpl extends ServiceImpl<LendReturnMapper, LendRet
         //还款编号
         String agentBatchNo = (String) paramMap.get("agentBatchNo");
 
+        boolean result = transFlowService.isSaveTransFlow(agentBatchNo);
+
+        if (result) {
+            log.warn("幂等性返回");
+            return;
+        }
+
+        //获取还款数据
+        QueryWrapper<LendReturn> lendReturnQueryWrapper = new QueryWrapper<>();
+        lendReturnQueryWrapper.eq("return_no", agentBatchNo);
+        LendReturn lendReturn = baseMapper.selectOne(lendReturnQueryWrapper);
+
+
+        //更新还款状态
+        String voteFeeAmt = (String) paramMap.get("voteFeeAmt");
+        lendReturn.setStatus(1);
+        lendReturn.setFee(new BigDecimal(voteFeeAmt));
+        lendReturn.setRealReturnTime(LocalDateTime.now());
+        baseMapper.updateById(lendReturn);
+
+
+        //更新标的信息
+        Lend lend = lendMapper.selectById(lendReturn.getLendId());
+        //如果是最后一次还款，那么久更新标的状态
+        if (lendReturn.getLast()) {
+            lend.setStatus(LendStatusEnum.PAY_OK.getStatus());
+            lendMapper.updateById(lend);
+        }
+
+        //还款账号转出金额
+        BigDecimal totalAmt = new BigDecimal((String) paramMap.get("totalAmt"));
+        String bindCode = userBindService.getBindCodeByUserId(lendReturn.getUserId());
+        userAccountMapper.updateAccount(bindCode, totalAmt.negate(), new BigDecimal(0));
+
+        //还款流水
+        TransFlowBO transFlowBO = new TransFlowBO(agentBatchNo,
+                bindCode,
+                totalAmt,
+                TransTypeEnum.RETURN_DOWN,
+                "借款人还款扣减，项目编号：" + lend.getLendNo() + "，项目名称：" + lend.getTitle());
+        transFlowService.saveTransFlow(transFlowBO);
+
+        //回款明细的获取
+        List<LendItemReturn> lendItemReturnList = lendItemReturnService.selectLendItemReturnList(lendReturn.getId());
+        lendItemReturnList.forEach(item -> {
+            //更新回款状态
+            item.setStatus(1);
+            item.setRealReturnTime(LocalDateTime.now());
+            lendItemReturnMapper.updateById(item);
+
+            //更新出借信息
+            LendItem lendItem = lendItemMapper.selectById(item.getLendItemId());
+            lendItem.setRealAmount(lendItem.getRealAmount().add(item.getInterest()));//动态的实际收益
+            lendItemMapper.updateById(lendItem);
+
+            //投资账户转入金额
+            String investBindCode = userBindService.getBindCodeByUserId(item.getInvestUserId());
+            userAccountMapper.updateAccount(investBindCode, item.getTotal(), new BigDecimal(0));
+
+
+            //回款流水
+            TransFlowBO investTransFlowBO = new TransFlowBO(
+                    LendNoUtils.getReturnItemNo(),
+                    investBindCode,
+                    item.getTotal(),
+                    TransTypeEnum.INVEST_BACK,
+                    "还款到账，项目编号：" + lend.getLendNo() + "，项目名称：" + lend.getTitle()
+            );
+            transFlowService.saveTransFlow(investTransFlowBO);
+        });
     }
 }
--- service-core/src/main/java/jw/srb/core/service/impl/LendServiceImpl.java
+++ service-core/src/main/java/jw/srb/core/service/impl/LendServiceImpl.java
@@ -5,6 +5,7 @@ package jw.srb.core.service.impl;/**
  */
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jw.srb.core.enums.LendStatusEnum;
 import jw.srb.core.mapper.BorrowerMapper;
 import jw.srb.core.mapper.LendMapper;
 import jw.srb.core.mapper.UserAccountMapper;
@@ -13,11 +14,15 @@ import jw.srb.core.pojo.entity.BorrowInfo;
 import jw.srb.core.pojo.entity.Lend;
 import jw.srb.core.pojo.vo.BorrowInfoApprovalVO;
 import jw.srb.core.service.*;
+import jw.srb.core.util.LendNoUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -62,11 +67,52 @@ public class LendServiceImpl extends ServiceImpl<LendMapper, Lend> implements Le
 
     @Override
     public void createLend(BorrowInfoApprovalVO borrowInfoApprovalVO, BorrowInfo borrowInfo) {
+        Lend lend = new Lend();
+        lend.setUserId(borrowInfo.getUserId());
+        lend.setBorrowInfoId(borrowInfo.getId());
+        lend.setLendNo(LendNoUtils.getLendNo());
+        lend.setTitle(borrowInfoApprovalVO.getTitle());
+        lend.setAmount(borrowInfo.getAmount());//标的金额
+        lend.setPeriod(borrowInfo.getPeriod());
+        lend.setLendYearRate(borrowInfoApprovalVO.getLendYearRate().divide(new BigDecimal(100)));
+        lend.setServiceRate(borrowInfoApprovalVO.getServiceRate().divide(new BigDecimal(100)));
+        lend.setReturnMethod(borrowInfo.getReturnMethod());
+        lend.setLowestAmount(new BigDecimal(100)); //最低投资金额
+        lend.setInvestAmount(new BigDecimal(0)); //已投金额
+        lend.setInvestNum(0); //已投人数
+        lend.setPublishDate(LocalDateTime.now());
 
+        //起息日期
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate lendStartDate = LocalDate.parse(borrowInfoApprovalVO.getLendStartDate(), dateTimeFormatter);
+        lend.setLendStartDate(lendStartDate);
+        //结束日期
+        LocalDate lendEndDate = lendStartDate.plusMonths(borrowInfo.getPeriod());
+        lend.setLendEndDate(lendEndDate);
+
+        //平台预期收益 = 标的金额 * (年化 / 12 * 期数)
+        BigDecimal monthRate = lend.getServiceRate().divide(new BigDecimal(12), 8, BigDecimal.ROUND_DOWN);
+        BigDecimal expectAmount = lend.getAmount().multiply(monthRate.multiply(new BigDecimal(lend.getPeriod())));
+        lend.setExpectAmount(expectAmount);
+
+        lend.setRealAmount(new BigDecimal(0));//实际收益
+        lend.setStatus(LendStatusEnum.INVEST_RUN.getStatus());//标的状态
+        lend.setCheckTime(LocalDateTime.now());//审核时间
+        lend.setCheckAdminId(1L);//审核人
+
+        baseMapper.insert(lend);
     }
 
     @Override
     public List<Lend> selectList() {
+
+        List<Lend> lendList = baseMapper.selectList(null);
+        lendList.forEach(lend -> {
+            String returnMethod = dictService.getNameByParentDictCodeAndValue("returnMethod", lend.getReturnMethod());
+            String status = LendStatusEnum.getMsgByStatus(lend.getStatus());
+            
+
+        });
         return null;
     }
 
